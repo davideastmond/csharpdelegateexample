d5d600ec646148a773f2da8283ff2510f2d98478
davideastmond <davebuck1981@gmail.com>
2018-12-20 23:02:43 -0500

    first

---
+++ Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharpDelegates
+{
+    public delegate void GetInfo(object sender, string e);
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Bring up our info form
+
+            secondForm newForm = new secondForm();
+            newForm.delegateProperty.OnInfoObtained += DelegateProperty_OnInfoObtained;
+            newForm.Show();
+        }
+
+        private void DelegateProperty_OnInfoObtained(object sender, string e)
+        {
+            Console.WriteLine("Delegate string is " + e);
+            
+        }
+    }
+}
---
+++ InfoObtained.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpDelegates
+{
+    public class InfoObtained
+    {
+        public event GetInfo OnInfoObtained;
+        public object p_sender;
+        public string p_EventArgs;
+
+        public InfoObtained()
+        {
+
+        }
+
+        public void RaiseEvent (object sender, string e)
+        {
+            p_sender = sender;
+            p_EventArgs = e;
+
+            OnInfoObtained += InfoObtained_OnInfoObtained;
+            OnInfoObtained(sender, e);
+        }
+
+        private void InfoObtained_OnInfoObtained(object sender, string e)
+        {
+            //throw new NotImplementedException();
+        }
+    }
+}
